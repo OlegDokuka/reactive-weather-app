f17f3dffda72e17480c0c578abc8ea909f294c75
Oleh Dokuka <shadowgun@i.ua>
2016-11-14 19:14:24 +0200

    add gatling performance test

---
+++ src/test/scala/com/example/benchmark/simulations/WeatherBenchmarkSimulation.scala
@@ -0,0 +1,42 @@
+package com.example.benchmark.simulations
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import scala.concurrent.duration._
+
+class WeatherBenchmarkSimulation extends Simulation {
+
+  val baseURL = Option(System.getProperty("baseURL")) getOrElse
+    (Option(System.getenv("BASE_URL")) getOrElse """http://localhost:8080""")
+
+  val httpConf = http
+    .baseURL(s"$baseURL/api/weather") // Here is the root for all relative URLs
+    .inferHtmlResources()
+    .acceptHeader("*/*")
+    .acceptEncodingHeader("gzip, deflate")
+    .acceptLanguageHeader("fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3")
+    .header("Connection", "keep-alive")
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:33.0) Gecko/20100101 Firefox/33.0")
+
+  val seq = Map(
+    "Belgium" -> "Brussels",
+    "USA" -> "Las Vegas"
+  ).map { case (k, v) => Map(
+    "country" -> k,
+    "city" -> v
+  )
+  }.toSeq
+
+  val scn = scenario("Weather App")
+    .repeat(1000) {
+      foreach(_ => seq, "location") {
+        exec(http("Get Weather Page")
+          .get("/now/${location.country}/${location.city}")
+          .check(status.is(200)))
+          .pause(1 millisecond)
+      }
+    }
+
+  setUp(scn.inject(atOnceUsers(100)).protocols(httpConf))
+}
